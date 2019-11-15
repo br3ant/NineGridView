564ca3d1aaaf8db4fbebefedc889dcf0e1e2d336
houqiqi <1106617567@qq.com>
2019-11-15 11:09:28 +0800

    修复onLayout重复调用

--- ninegridview/src/main/java/com/lzy/ninegrid/NineGridView.java
+++ ninegridview/src/main/java/com/lzy/ninegrid/NineGridView.java
@@ -104,9 +104,9 @@ public class NineGridView extends ViewGroup {
             int bottom = top + gridHeight;
             childrenView.layout(left, top, right, bottom);
 
-            if (mImageLoader != null) {
-                mImageLoader.onDisplayImage(getContext(), childrenView, mImageInfo.get(i).thumbnailUrl);
-            }
+//            if (mImageLoader != null) {
+//                mImageLoader.onDisplayImage(getContext(), childrenView, mImageInfo.get(i).thumbnailUrl);
+//            }
         }
     }
 
